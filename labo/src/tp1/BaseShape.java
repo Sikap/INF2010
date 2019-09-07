8f7e498cf7b73bb695413de121f7b6d621def4ef
Alphonse Desrosiers <alp-on_des@hotmail.com>
2019-09-07 15:57:24 -0400

    avec IntelliJ jarrive a 8.0 que avec eclise j'ai 5.5 on reste avec Intellij

--- labo/src/tp1/BaseShape.java
+++ labo/src/tp1/BaseShape.java
@@ -10,61 +10,72 @@ public class BaseShape {
 
     // TODO Initialiser les points.
     public BaseShape() {
-        // ...
+       // Set<Point2d> coords= new HashSet<Point2d>();
     }
 
     // TODO prendre une liste de points et creer une nouvelle forme.
     public BaseShape(Collection<Point2d> coords) {
-        // ...
+    	//this.coords=(Set<Point2d>)coords;	
     }
 
     // TODO ajouter ou retirer des coordonnees a la liste de points.
     public void add(Point2d coord) {
-        // ...
+        this.coords.add(coord);
     }
     public void add(BaseShape shape) {
-        // ...
+        this.add(shape);
     }
     public void addAll(Collection<Point2d> coords) {
-        // ...
+     //   this.addAll(coords);
     }
     public void remove(Point2d coord) {
-        // ...
+    //  this.coords.remove(coord);
     }
     public void remove(BaseShape shape) {
-        // ...
+    //    this.remove(shape);
     }
     public void removeAll(Collection<Point2d> coords) {
-        // ...
+     //   this.removeAll(coords);
     }
 
     // TODO retourne les coordonnees de la liste.
     public Set<Point2d> getCoords() {
-        return null;
+        return this.coords;
     }
 
     // TODO appliquer la translation sur la forme.
     public BaseShape translate(Point2d point) {
+        
+        //his.coords= new Point2d(this.coords.X()+point.X(),this.coords.Y()+point.Y());
         return null;
     }
 
     // TODO appliquer la translation sur la liste.
     public Set<Point2d> translateAll(Point2d point) {
-        return null;
+        //this.coords= new Point2d(this.coords.X()+point.X(),this.coords.Y()+point.Y());
+        return this.coords;
     }
 
     // TODO appliquer la rotation sur la forme.
     public BaseShape rotate(Double angle) {
-        return null;
+      /*  Double XRotation=Math.cos(angle)*this.X()+sin(angle)*this.Y();
+        Double YRotation=Math.sin(angle)*this.X()+cos(angle)*this.Y();
+        this.coords.vecteur[0]=XRotation;
+        this.coords.vecteur[1]=YRotationl;*/
+        return this;
     }
 
     // TODO appliquer la rotation sur la liste.
     public Set<Point2d> rotateAll(Double angle) {
-        return null;
+        /*Double XRotation=Math.cos(angle)*this.X()+sin(angle)*this.Y();
+        Double YRotation=Math.sin(angle)*this.X()+cos(angle)*this.Y();
+        this.coords.vecteur[0]=XRotation;
+        this.coords.vecteur[1]=YRotationl;*/
+        return this.coords;
     }
 
     // TODO retourner une nouvelle forme.
     public BaseShape clone() {
-        return this;
+        return  new BaseShape(this.coords);
     }
 }
--- labo/src/tp1/PointOperator.java
+++ labo/src/tp1/PointOperator.java
@@ -5,27 +5,54 @@ import java.util.*;
 public final class PointOperator {
     // TODO appliquer la translation sur le vecteur d'entree.
     public static Double[] translate(Double[] vector, Double[] translateVector) {
-        return null;
+        int lengthVector=vector.length;
+        Double[] tramslte =new Double[lengthVector];
+        for (int i=0;i<lengthVector;i++){
+            tramslte[i]=vector[i]+translateVector[i];
+        }
+        return tramslte;
     }
 
     // TODO appliquer la rotation sur le vecteur d'entree.
     public static Double[] rotate(Double[] vector, Double[][] rotationMatrix) {
-        return null;
+    	Double[] tramslte ={};
+    	for(int i=0;i<3;i++){tramslte[i]=0.0;}
+    	for(int i=0;i<3;i++){
+    		for(int j=0;j<3;j++){
+    			tramslte[i]=tramslte[i]+(vector[j]*rotationMatrix[i][j]);
+    		}
+    	}
+        return tramslte;
     }
-
+        
     // TODO appliquer le facteur de division sur le vecteur d'entree.
     public static Double[] divide(Double[] vector, Double divider) {
-        return null;
+    	int lengthVector=vector.length;
+    	Double[] tramslte =new Double[lengthVector];
+    	for(int i=0;i<lengthVector;i++){
+    		tramslte[i]=vector[i]/divider;
+    	}
+        return tramslte;
     }
 
     // TODO appliquer le facteur de multiplication sur le vecteur d'entree.
     public static Double[] multiply(Double[] vector, Double multiplier) {
-        return null;
+    	int lengthVector=vector.length;
+    	Double[] tramslte =new Double[lengthVector];
+    	for(int i=0;i<lengthVector;i++){
+    		tramslte[i]=vector[i]*multiplier;
+    	}
+        return tramslte;
     }
 
     // TODO appliquer le facteur d'addition sur le vecteur d'entree.
     public static Double[] add(Double[] vector, Double adder) {
-        return null;
+    	int lengthVector=vector.length;
+    	Double[] tramslte =new Double[lengthVector];
+    	for(int i=0;i<lengthVector;i++){
+    		tramslte[i]=vector[i]+adder;
+    	}
+        return tramslte;
     }
 
     // TODO retourne la coordonnee avec les plus grandes valeurs en X et en Y.
