686f3de6e3e718cbe8b04af0a83dba234695b39a
Alphonse Desrosiers <alp-on_des@hotmail.com>
2019-09-09 18:07:19 -0400

    Point2d et Point Operator 10/10 pour les teste

--- labo/src/tp1/Point2d.java
+++ labo/src/tp1/Point2d.java
@@ -21,60 +21,56 @@ public class Point2d extends AbstractPoint {
     //a veriffier plus tart si return un new point est une erreur
     @Override
     public Point2d translate(Double[] translateVector) {
-        vector[X]=this.X()+translateVector[X];
-        vector[Y]=this.Y()+translateVector[Y];
-        return this;
+        Double xTranslation=this.X()+translateVector[X];
+        Double yTranslation=this.Y()+translateVector[Y];
+        return new Point2d(xTranslation,yTranslation);
     }
 
     // TODO prendre un point et appliquer la translation.
     //a veriffier plus tart si return un new point est une erreur
     public Point2d translate(Point2d translateVector) {
-        vector[X]=this.X()+translateVector.X();
-        vector[Y]=this.Y()+translateVector.Y();
-        return this;
+        Double xTranslation=this.X()+translateVector.X();
+        Double yTranslation=this.Y()+translateVector.Y();
+        return new Point2d(xTranslation,yTranslation);
     }
 
     // TODO prendre un vecteur de donnees et appliquer la translation.
     @Override
     public Point2d rotate(Double[][] rotationMatrix) {
-        Double xTranslation=rotationMatrix[0][0]*this.X()-rotationMatrix[0][1]*this.Y();
-        Double yTranslation=rotationMatrix[1][0]*this.X()+rotationMatrix[1][1]*this.Y();
-        vector[X]=xTranslation;
-        vector[Y]=yTranslation;
-        return this;
+        Double xRotate=rotationMatrix[0][0]*this.X()-rotationMatrix[0][1]*this.Y();
+        Double yRotate=rotationMatrix[1][0]*this.X()+rotationMatrix[1][1]*this.Y();
+        return new Point2d(xRotate,yRotate);
     }
 
     // TODO prendre un angle de rotation, creer une matrice et appliquer la rotation.
     public Point2d rotate(Double angle) {
-        Double xTranslation=this.X()*Math.cos(angle)-this.Y()*Math.sin(angle);
-        Double yTranslation=this.X()*Math.sin(angle)+this.Y()*Math.cos(angle);
-        vector[X]=xTranslation;//transforme mon double en int
-        vector[Y]=yTranslation;
-        return this;
+        Double xRotate=this.X()*Math.cos(angle)-this.Y()*Math.sin(angle);
+        Double yRotate=this.X()*Math.sin(angle)+this.Y()*Math.cos(angle);
+        return new Point2d(xRotate,yRotate);
     }
 
     // TODO prendre un facteur de division et l'appliquer.
     @Override
     public Point2d divide(Double divider) {
-        vector[X]=X()/divider;
-        vector[Y]=Y()/divider;
-        return this;
+        Double xDiv=X()/divider;
+        Double yDiv=Y()/divider;
+        return new Point2d(xDiv,yDiv);
     }
 
     // TODO prendre un facteur de multiplication et l'appliquer.
     @Override
     public Point2d multiply(Double multiplier) {
-        vector[X]=X()*multiplier;
-        vector[Y]=Y()*multiplier;
-        return this;
+        Double xMul=X()*multiplier;
+        Double yMul=Y()*multiplier;
+        return new Point2d(xMul,yMul);
     }
 
     // TODO prendre un facteur d'addition et l'appliquer.
     @Override
     public Point2d add(Double adder) {
-        vector[X]=X()+adder;
-        vector[Y]=Y()+adder;
-        return this;
+        Double xAdd=X()+adder;
+        Double yAdd=Y()+adder;
+        return new Point2d(xAdd,yAdd);
     }
 
     // TODO creer un nouveau point.
