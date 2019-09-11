b548f839c0873c43774039f60ba2dc82443127e3
Sikap <49215307+Sikap@users.noreply.github.com>
2019-09-11 16:23:02 -0400

    12/20

--- labo/src/tp1/BaseShape.java
+++ labo/src/tp1/BaseShape.java
@@ -10,67 +10,73 @@ public class BaseShape {
 
     // TODO Initialiser les points.
     public BaseShape() {
-       // Set<Point2d> coords= new HashSet<Point2d>();
+       Set<Point2d> coords= new HashSet<Point2d>();
     }
 
     // TODO prendre une liste de points et creer une nouvelle forme.
     public BaseShape(Collection<Point2d> coords) {
-    	//this.coords=(Set<Point2d>)coords;	
+        this.coords= new HashSet<Point2d>(coords);
     }
+
 
     // TODO ajouter ou retirer des coordonnees a la liste de points.
-    public void add(Point2d coord) {
-        this.coords.add(coord);
-    }
+    public void add(Point2d coord) {this.coords.add(coord); }
     public void add(BaseShape shape) {
-        this.add(shape);
+        this.coords.addAll(shape.coords);
     }
-    public void addAll(Collection<Point2d> coords) {
-     //   this.addAll(coords);
-    }
-    public void remove(Point2d coord) {
-    //  this.coords.remove(coord);
-    }
+    public void addAll(Collection<Point2d> coords) { this.coords.addAll(coords); }
+    public void remove(Point2d coord) { this.coords.remove(coord); }
     public void remove(BaseShape shape) {
-    //    this.remove(shape);
+
+            this.coords.removeAll(shape.coords);
     }
-    public void removeAll(Collection<Point2d> coords) {
-     //   this.removeAll(coords);
-    }
+    public void removeAll(Collection<Point2d> coords) { this.coords.removeAll(coords); }
 
     // TODO retourne les coordonnees de la liste.
-    public Set<Point2d> getCoords() {
-        return this.coords;
-    }
+    public Set<Point2d> getCoords() {return this.coords; }
 
     // TODO appliquer la translation sur la forme.
     public BaseShape translate(Point2d point) {
-        
-        //his.coords= new Point2d(this.coords.X()+point.X(),this.coords.Y()+point.Y());
-        return null;
+
+        Iterator<Point2d> itr=this.coords.iterator();
+        while (itr.hasNext()) {
+            itr.next().translate(point);
+        }
+        return this;
     }
 
     // TODO appliquer la translation sur la liste.
     public Set<Point2d> translateAll(Point2d point) {
-        //this.coords= new Point2d(this.coords.X()+point.X(),this.coords.Y()+point.Y());
+        Iterator<Point2d> itr=this.coords.iterator();
+        while (itr.hasNext()) {
+            itr.next().translate(point);
+        }
         return this.coords;
     }
 
     // TODO appliquer la rotation sur la forme.
     public BaseShape rotate(Double angle) {
-      /*  Double XRotation=Math.cos(angle)*this.X()+sin(angle)*this.Y();
-        Double YRotation=Math.sin(angle)*this.X()+cos(angle)*this.Y();
-        this.coords.vecteur[0]=XRotation;
+       /*Double XRotation=Math.cos(angle)*this.X()+sin(angle)*this.Y();
+       Double YRotation=Math.sin(angle)*this.X()+cos(angle)*this.Y();
+        this.coords.vecteur[0]=XRotatio;
         this.coords.vecteur[1]=YRotationl;*/
+        Iterator<Point2d> itr=this.coords.iterator();
+        while (itr.hasNext()) {
+         itr.next().rotate(angle);
+        }
         return this;
     }
 
     // TODO appliquer la rotation sur la liste.
     public Set<Point2d> rotateAll(Double angle) {
-        /*Double XRotation=Math.cos(angle)*this.X()+sin(angle)*this.Y();
+        /* Double XRotation=Math.cos(angle)*this.X()+sin(angle)*this.Y();
         Double YRotation=Math.sin(angle)*this.X()+cos(angle)*this.Y();
         this.coords.vecteur[0]=XRotation;
-        this.coords.vecteur[1]=YRotationl;*/
+        this.coords.vecteur[1]=YRotation;*/
+        Iterator<Point2d> itr=this.coords.iterator();
+        while (itr.hasNext()) {
+         itr.next().rotate(angle);
+        }
         return this.coords;
     }
 
--- labo/src/tp1/Rectangle.java
+++ labo/src/tp1/Rectangle.java
@@ -1,16 +1,22 @@
 package tp1;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Rectangle extends BaseShape {
     // TODO creer un rectangle avec une largeur et une longueur.
     public Rectangle(Double width, Double height) {
-        // ...
+        Set<Point2d> dimensions = new HashSet<Point2d>();
+        Point2d point = new Point2d(width,height);
+        dimensions.add(point);
+        new Rectangle(dimensions);
     }
 
     // TODO creer un rectangle avec un point contenant la largeur et longueur.
     public Rectangle(Point2d dimensions) {
-        // ...
+        Set<Point2d> coords =new HashSet<Point2d>();
+        coords.add(dimensions);
+        new Rectangle(coords);
     }
 
     private Rectangle(Set<Point2d> coords) {
@@ -20,18 +26,14 @@ public class Rectangle extends BaseShape {
     // TODO appliquer la translation sur la forme.
     @Override
     public Rectangle translate(Point2d point) {
-        return null;
+        return new Rectangle(translateAll(point));
     }
 
     // TODO appliquer la rotation sur la forme.
     @Override
-    public Rectangle rotate(Double angle) {
-        return null;
-    }
+    public Rectangle rotate(Double angle) { return new Rectangle(rotateAll(angle)); }
 
     // TODO retourner une nouvelle forme.
     @Override
-    public Rectangle clone() {
-        return null;
-    }
+    public Rectangle clone() {return  new Rectangle(getCoords()); }
 }
