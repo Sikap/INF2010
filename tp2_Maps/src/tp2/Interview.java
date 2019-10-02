e45c880210eeec176bc53177184361228920506f
Sikap <49215307+Sikap@users.noreply.github.com>
2019-10-02 19:43:56 -0400

    FINI

--- tp2_Maps/src/tp2/Interview.java
+++ tp2_Maps/src/tp2/Interview.java
@@ -13,36 +13,30 @@ public class Interview {
      * @return A collection containing all valid pairs with no permutations, but all combinations (empty collection if none found)
      */
 
-    public Collection<MatchingPair> matchingPairs(Collection<Integer> values, Integer targetSum){
-        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
-        Collection<MatchingPair> Pairs= new ArrayList<>();
-        Integer[] Values = values.toArray(new Integer[values.size()]);
+    public Collection<MatchingPair> matchingPairs(Collection<Integer> values, Integer targetSum) {
 
-        for(int i=0;i<Values.length;i++)//Mettre les Values dans la map
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Collection<MatchingPair> Pairs = new ArrayList<>();
+        Integer[] Values = values.toArray(new Integer[values.size()]);
+        for (int i = 0; i < Values.length; i++)
         {
 
-            if(map.get(Values[i])==null)
+            if (map.get(Values[i]) == null)
             {
                 map.put(Values[i], i);
             }
 
         }
-        for(int i=0;i<Values.length;i++)
-        {
-           if(map.get(targetSum-Values[i])!=null && map.get(targetSum-Values[i])!=i)
-            {
-                if(!Pairs.contains(new MatchingPair(targetSum - Values[i],Values[i])))
-                {
+        for (int i = 0; i < Values.length; i++) {
+            if (map.get(targetSum - Values[i]) != null && map.get(targetSum - Values[i]) != i) {
+
+                if (!Pairs.contains(new MatchingPair(targetSum - Values[i], Values[i]))) {
                     Pairs.add(new MatchingPair(Values[i], targetSum - Values[i]));
                 }
             }
-
         }
-
-
         return Pairs;
+
     }
-
-
 }
 
