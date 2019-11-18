9d02f42a8fe513c202a677fcceb729c976727317
Simon Kaplo <simon.kaplo@polymtl.ca>
2019-11-18 17:46:07 -0500

    findpath fonctione

--- tp5_Code/src/Dijkstra.java
+++ tp5_Code/src/Dijkstra.java
@@ -1,9 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Stack;
-
+import java.util.*;
 
 
 public class Dijkstra {
@@ -11,27 +6,39 @@ public class Dijkstra {
 	private Graph graph;
 	private Map<Node, Edge> dijkstraTable[];
 	private Stack<Edge> path;
-	
+
 
 	public Dijkstra (Graph g) {
 		this.graph = g;
 	}
 
 	public void findPath(Node s, Node d) {
+		int nbNode=graph.getNodes().size();
+		dijkstraTable = new HashMap[nbNode];
+		Node curentNode = s;
+		// First Iteration
+		dijkstraTable[0]= new HashMap<>();
+		dijkstraTable[0].put(s,new Edge(s,s,0));
+		// N Iteration
+		int index =1;
+		Set <Node> visitedNodes = new HashSet<>();
+		while (curentNode!=d) {
+			visitedNodes.add(curentNode);
+			dijkstraTable[index] = new HashMap<>(dijkstraTable[index-1]);
+			dijkstraTable[index].remove(curentNode);
+			List<Edge> edges = graph.getEdgesGoingFrom(curentNode);
+			for (Edge i: edges)
+			{
+				Edge e = new Edge(i.getSource(),i.getDestination(),i.getDistance()+dijkstraTable[index-1].getOrDefault(curentNode,new Edge(i.getSource(),i.getDestination(),0)).getDistance());
+				boolean isCurrentNodeVisited = visitedNodes.contains(i.getDestination());
+				boolean hasRoute = dijkstraTable[index-1].containsKey(i.getDestination());
+				if(!isCurrentNodeVisited && (!hasRoute || ( hasRoute &&  dijkstraTable[index - 1].get(i.getDestination()).getDistance() > e.getDistance() )))
+				dijkstraTable[index].put(e.getDestination(),e);
+			}
 
-		dijkstraTable = new HashMap[graph.getNodes().size()];
-
-		List <Edge> edge = graph.getEdgesGoingFrom(s);
-		//dijkstraTable[0].put(s,);
-		for(int i=1;i<dijkstraTable.length;i++) {
-
-			//path.push()
-			//dijkstraTable[i].put(,0);
-
+			curentNode = getMinimum(dijkstraTable[index]);
+			index++;
 		}
-		// A compléter
-
-		
 	}
 
 	private Node getMinimum(Map<Node, Edge> map) {
